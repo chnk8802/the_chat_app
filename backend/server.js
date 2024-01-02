15838255fc31403eda7e1a31b9887c3ffa848333
Naveen <naveen@localhost.localdomain>
2024-01-03 00:30:42 +0530

    Rbe-8

--- backend/server.js
+++ backend/server.js
@@ -23,16 +23,16 @@ app.use('/api/message', messageRoutes);
 
 // ------------Deployment-----------------
 const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, '/frontend/dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, 'frontend', 'dist', 'index.html'));
-    })
-} else {
-    app.get('/', (req, res) => {
-        res.send("API Running successfully!!!!!!")
-    });
-}
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname1, '/frontend/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname1, 'frontend', 'dist', 'index.html'));
+});
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send("API Running successfully!!!!!!")
+//     });
+// }
 // ------------Deployment-----------------
 
 
